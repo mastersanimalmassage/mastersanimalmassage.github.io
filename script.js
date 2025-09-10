c282483018d210cedb5942ae5804366c00dac64e
lamasters <liam.masters3@gmail.com>
2025-09-10 12:35:28 -0400

    Update price and location

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
-var about_button = document.getElementById("about-button");
-var about_section = document.getElementById("tag-container");
-var services_button = document.getElementById("services-button");
-var services_section = document.getElementById("desc-container");
 var inquire_button = document.getElementById("inquire-nav");
 var inquire_section = document.getElementById("book-container");
 
 const scroll = (div) => {
-    div.scrollIntoView({behavior: "smooth"});
-}
+  div.scrollIntoView({ behavior: "smooth" });
+};
 
-about_button.onclick = function() {scroll(about_section)};
-services_button.onclick = function() {scroll(services_section)};
-inquire_button.onclick = function() {scroll(inquire_section)};
+inquire_button.onclick = function () {
+  scroll(inquire_section);
+};
